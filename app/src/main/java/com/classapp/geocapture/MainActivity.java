7ebacf04374b721ba6b1bf12dd94ee2030ac09fa
RedwanSharafatKabir <mohammadmunna478@gmail.com>
2021-07-19 15:26:45 +0600

    Camera with device lat lng

--- app/src/main/java/com/classapp/geocapture/MainActivity.java
+++ app/src/main/java/com/classapp/geocapture/MainActivity.java
@@ -1,64 +1,75 @@
 package com.classapp.geocapture;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
-import android.media.audiofx.BassBoost;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.provider.Settings;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.TimeZone;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private LinearLayout imageLayout;
+    private Button captureBtn;
+    private ImageView capturedImage, finalImage;
     private TextView locationOutput;
     private String latitude = "", longitude = "", currentDate, currentTime, currentPlace;
     private FusedLocationProviderClient fusedLocationProviderClient;
+    private int CAPTURE_REQUEST_CODE = 101, CAPTURE_CODE = 102;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        captureBtn = findViewById(R.id.captureAnotherId);
+        captureBtn.setOnClickListener(this);
+        imageLayout = findViewById(R.id.imageLinearLauoutId);
+        capturedImage = findViewById(R.id.capturedImageId);
         locationOutput = findViewById(R.id.locationOutputId);
+        finalImage = findViewById(R.id.finalImageId);
+        finalImage.setVisibility(View.GONE);
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
 
@@ -78,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
         }
 
@@ -90,17 +101,19 @@ public class MainActivity extends AppCompatActivity {
         }).addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
-                latitude = String.valueOf(location.getLatitude());
-                longitude = String.valueOf(location.getLongitude());
+                if(location!=null) {
+                    latitude = String.valueOf(location.getLatitude());
+                    longitude = String.valueOf(location.getLongitude());
 
-                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
-                List<Address> addressList;
+                    Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                    List<Address> addressList;
 
-                try {
-                    addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                    currentPlace = addressList.get(0).getLocality();
-                } catch (IOException e) {
-                    Log.i("ERROR ", "Permission Denied");
+                    try {
+                        addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                        currentPlace = addressList.get(0).getLocality();
+                    } catch (IOException e) {
+                        Log.i("ERROR ", "Permission Denied");
+                    }
                 }
 
                 locationOutput.setText("Date:" + currentDate);
@@ -110,6 +123,109 @@ public class MainActivity extends AppCompatActivity {
                 locationOutput.append("\nLongitude:" + longitude + getResources().getString(R.string.degreeSymbol));
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.captureAnotherId){
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE}, CAPTURE_REQUEST_CODE);
+            }
+            else {
+                openCamera();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAPTURE_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(MainActivity.this, "Camera permission is required", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void openCamera(){
+        Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(captureIntent, CAPTURE_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAPTURE_CODE){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            capturedImage.setImageBitmap(bitmap);
+
+            Bitmap image = getBitmapFromView(imageLayout);
+            imageLayout.setVisibility(View.GONE);
+            finalImage.setVisibility(View.VISIBLE);
+            finalImage.setImageBitmap(image);
+
+            saveImageInGallery();
+        }
+    }
+
+    private void saveImageInGallery(){
+        FileOutputStream outputStream = null;
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        if (!dir.exists()){
+            dir.mkdir();
+        }
+
+        BitmapDrawable drawable = (BitmapDrawable) finalImage.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        File file = new File(dir,System.currentTimeMillis() + ".jpg");
+
+        try {
+            outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        } catch (Exception e) {
+            Log.i("ERROR ", e.getMessage());
+        }
+
+        Toast.makeText(MainActivity.this,"Successfuly Saved",Toast.LENGTH_SHORT).show();
+
+        try {
+            outputStream.flush();
+        } catch (Exception e) {
+            Log.i("ERROR ", e.getMessage());
+        }
+
+        try {
+            outputStream.close();
+        } catch (Exception e) {
+            Log.i("ERROR ", e.getMessage());
+        }
+    }
+
+    private Bitmap getBitmapFromView(View view) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable!=null) {
+            bgDrawable.draw(canvas);
+        }   else{
+            canvas.drawColor(Color.WHITE);
+        }
+
+        view.draw(canvas);
+
+        return returnedBitmap;
     }
 
     // Another method to get current location
