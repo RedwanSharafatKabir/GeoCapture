310a47cf5c94892a83c2f8cc6f8fdba8942760b1
RedwanSharafatKabir <mohammadmunna478@gmail.com>
2021-07-22 23:00:37 +0600

    Camera with device lat lng

--- app/src/main/java/com/classapp/geocapture/MainActivity.java
+++ app/src/main/java/com/classapp/geocapture/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -17,8 +19,10 @@ import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -26,15 +30,18 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -45,19 +52,18 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ProgressBar progressBar;
-    private LinearLayout imageLayout;
-    private Button captureBtn;
-    private ImageView capturedImage, finalImage;
+    private FrameLayout imageLayout;
+    private ImageView capturedImage, finalImage, captureBtn;
     private TextView locationOutput;
     private String latitude = "", longitude = "", currentDate, currentTime, currentPlace;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private int CAPTURE_REQUEST_CODE = 101, CAPTURE_CODE = 102;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -88,10 +94,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         currentTime = simpleDateFormat2.format(new Date());
 
 //        getlocation();
-        getCurrentLocation();
+        requestLocation();
     }
 
-    private void getCurrentLocation() {
+    private void requestLocation() {
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -99,40 +105,85 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
 
+            getCurrentLocation();
+
         } else {
-            fusedLocationProviderClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(MainActivity.this, "Location permission denied !", Toast.LENGTH_LONG).show();
-                }
-            }).addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if(location!=null) {
-                        latitude = String.valueOf(location.getLatitude());
-                        longitude = String.valueOf(location.getLongitude());
-
-                        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
-                        List<Address> addressList;
-
-                        try {
-                            addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                            currentPlace = addressList.get(0).getLocality();
-                        } catch (IOException e) {
-                            Log.i("ERROR ", "Permission Denied");
-                        }
-
-                        locationOutput.setText("Date:" + currentDate);
-                        locationOutput.append("\nTime:" + currentTime);
-                        locationOutput.append("\nPlace:" + currentPlace);
-                        locationOutput.append("\nLatitude:" + latitude + getResources().getString(R.string.degreeSymbol));
-                        locationOutput.append("\nLongitude:" + longitude + getResources().getString(R.string.degreeSymbol));
-
-                        progressBar.setVisibility(View.GONE);
-                    }
-                }
-            });
+            getCurrentLocation();
         }
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestLocation();
+        }
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        String provider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        latitude = String.valueOf(location.getLatitude());
+        longitude = String.valueOf(location.getLongitude());
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        List<Address> addressList;
+
+        try {
+            addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            currentPlace = addressList.get(0).getAddressLine(0);
+        } catch (IOException e) {
+            Log.i("ERROR ", "Permission Denied");
+        }
+
+        locationOutput.setText("Date: " + currentDate);
+        locationOutput.append("\nTime: " + currentTime);
+        locationOutput.append("\n\nAddress: " + currentPlace);
+//        locationOutput.append("\n\nLatitude: " + latitude + getResources().getString(R.string.degreeSymbol));
+//        locationOutput.append("\nLongitude: " + longitude + getResources().getString(R.string.degreeSymbol));
+
+        progressBar.setVisibility(View.GONE);
+
+/*
+        fusedLocationProviderClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Location permission denied !", Toast.LENGTH_LONG).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    latitude = String.valueOf(location.getLatitude());
+                    longitude = String.valueOf(location.getLongitude());
+
+                    Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                    List<Address> addressList;
+
+                    try {
+                        addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                        currentPlace = addressList.get(0).getLocality();
+                    } catch (IOException e) {
+                        Log.i("ERROR ", "Permission Denied");
+                    }
+
+                    locationOutput.setText("Date:" + currentDate);
+                    locationOutput.append("\nTime:" + currentTime);
+                    locationOutput.append("\nPlace:" + currentPlace);
+                    locationOutput.append("\nLatitude:" + latitude + getResources().getString(R.string.degreeSymbol));
+                    locationOutput.append("\nLongitude:" + longitude + getResources().getString(R.string.degreeSymbol));
+
+                    progressBar.setVisibility(View.GONE);
+                }
+
+            }
+        });
+ */
+
     }
 
     @Override
@@ -265,94 +316,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
     }
-
-    // Another method to get current location
-/*
-    private void getlocation(){
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            getFinalLocation();
-        }
-        else {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode==100 && grantResults.length>0 && (grantResults[0]+grantResults[1])==PackageManager.PERMISSION_GRANTED){
-            getFinalLocation();
-        }
-        else {
-            Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
-        }
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    @SuppressLint("MissingPermission")
-    private void getFinalLocation(){
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
-
-            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    Location location = task.getResult();
-                    latitude = String.valueOf(location.getLatitude());
-                    longitude = String.valueOf(location.getLongitude());
-
-                    if(location!=null){
-                        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
-                        List<Address> addressList;
-
-                        try {
-                            addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                            currentPlace = addressList.get(0).getLocality();
-                        } catch (IOException e) {
-                            Log.i("ERROR ", "Permission Denied");
-                        }
-
-                        locationOutput.setText("Date: " + currentDate);
-                        locationOutput.append("\nTime: " + currentTime);
-                        locationOutput.append("\nPlace: " + currentPlace);
-                        locationOutput.append("\nLatitude: " + latitude + getResources().getString(R.string.degreeSymbol));
-                        locationOutput.append("\nLongitude: " + longitude + getResources().getString(R.string.degreeSymbol));
-                    } else {
-                        LocationRequest locationRequest = new LocationRequest()
-                                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                                .setInterval(10000)
-                                .setNumUpdates(1);
-
-                        LocationCallback locationCallback = new LocationCallback() {
-                            @Override
-                            public void onLocationResult(@NonNull LocationResult locationResult) {
-                                Location location1 = locationResult.getLastLocation();
-
-                                latitude = String.valueOf(location1.getLatitude());
-                                longitude = String.valueOf(location1.getLongitude());
-
-                                locationOutput.setText("Date: " + currentDate);
-                                locationOutput.append("\nTime: " + currentTime);
-                                locationOutput.append("\nPlace: " + currentPlace);
-                                locationOutput.append("\nLatitude: " + latitude + getResources().getString(R.string.degreeSymbol));
-                                locationOutput.append("\nLongitude: " + longitude + getResources().getString(R.string.degreeSymbol));
-                            }
-                        };
-
-                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
-                    }
-                }
-            });
-        } else {
-            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS).
-                    setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-        }
-    }
-//*/
 }
